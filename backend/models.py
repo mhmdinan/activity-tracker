a3146eb3a5780f230714000d6a63a5e4853daecd
mhmdinan <muhammadinan41@gmail.com>
2026-01-17 17:10:13 +0500

    change count in main activity table to goals

--- backend/models.py
+++ backend/models.py
@@ -10,7 +10,7 @@ class Daily_Activity(base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=True)
-    count: Mapped[int] = mapped_column(nullable=False)
+    goal: Mapped[int] = mapped_column(nullable=False)
     logs: Mapped[List["Daily_Log"]] = relationship(back_populates="activity")
 
 class Daily_Log(base):
